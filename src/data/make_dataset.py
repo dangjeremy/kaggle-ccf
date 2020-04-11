a36a7fce763767c2edb6e97b2029ddaf5991f437
ah12068 <dang.jeremy01@gmail.com>
2020-04-11 17:58:21 +0100

    approved

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 import click
 import logging
+import kaggle
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
-
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+@click.option('-data', prompt='Dataset')
+def main(data):
+    """ Runs data query and places data in ../raw
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
-
+    logger.info('Querying dataset and unzipping')
+    kaggle.api.dataset_download_files(data, path='../../data/raw/', unzip=True)
+    logger.info('Data query complete.')
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
