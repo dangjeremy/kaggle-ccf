daf4f926840fa37b35ad5bd556a0a2808ae74fac
ah12068 <dang.jeremy01@gmail.com>
2020-04-11 18:28:09 +0100

    added jupyter setup file

---
+++ jupyter_setup.sh
@@ -0,0 +1,9 @@
+jupyter contrib nbextension install --user
+jupyter nbextensions_configurator enable --user
+
+jupyter nbextension enable codefolding/main
+jupyter nbextension enable execute_time/ExecuteTime
+jupyter nbextension enable varInspector/main
+jupyter nbextension enable collapsible_headings/main
+jupyter nbextension enable latex_envs/latex_envs
+jupyter nbextension enable codefolding/edit
